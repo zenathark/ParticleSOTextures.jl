b1b480fcc6567e68ebb34e7623910985e0b7f967
Juan Carlos Galan Hernandez <jcgalanhlanh@gmail.com>
2019-09-24 17:12:01 -0500

    Documenting Particle structures

--- src/types.jl
+++ src/types.jl
@@ -1,16 +1,26 @@
+"A Particle Swarm Optimization particle"
 struct Particle
+    "particle's speed"
     speed::Int
+    "particle's current position over a given image"
     pos::NamedTuple{(:x, :y),Tuple{Int,Int}}
+    "particle's direction"
     dir::NamedTuple{(:x, :y),Tuple{Int,Int}}
+    "particle's window pixel dimentions on x and y"
     size::NamedTuple{(:w, :h),Tuple{Int,Int}}
 end
 
-Particle(x, y, size) = Particle(0, (x = x, y = y), (x = 0, y = 0, size))
+"Creates a new particle initializing speed to 0, direction to 0,0, position to x,y and size to size"
+Particle(x, y, size) = Particle(0, (x = x, y = y), (x = 0, y = 0), size)
 
 struct ParticleList
     particles::Array{Particle,1}
     size::Int
+    dim::NamedTuple{(:w, :h),Tuple{Int,Int}}
 end
+
+ParticleList(particles) = 
+    ParticleList(particles, particles[0].size[:w] * particles[0].size[:h], particles[0].size)
 
 struct CoocurrenceMatrix
     coocurrence::Array{UInt}
@@ -22,8 +32,8 @@ CoocurrenceMatrix(Δq) = CoocurrenceMatrix(zeros(UInt, Δq, Δq), Δq)
 struct CoocurrenceMatrixGroup
     coocurrence::Array{UInt,3}
     Δq::Int
-    particle_size::Int
+    particles::ParticleList
 end
 
-CoocurrenceMatrixGroup(Δq, particle_size) = 
-    CoocurrenceMatrixGroup(zeros(UInt, particle_size, Δq, Δq), Δq, particle_size)
+CoocurrenceMatrixGroup(Δq, particles::ParticleList) = 
+    CoocurrenceMatrixGroup(zeros(UInt, particles.dim, Δq, Δq), Δq, particles)
